3e7aed6d7e2b10da4f86965ee4067f2d2dfc1f4d
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-07-02 05:00:39 -0500

    Updated task 7 fixed txt location

--- pong-one/Assets/Scripts/ScoreUIScript.cs
+++ pong-one/Assets/Scripts/ScoreUIScript.cs
@@ -20,39 +20,29 @@ public class ScoreUIScript : MonoBehaviour
         }
 
         // Create LeftScore GameObject
-        GameObject leftScore = CreateScoreObject("LeftScore", new Vector3(-100, -100, 0), canvas.transform);
+        CreateScoreObject("LeftScore", new Vector3(-100, -100, 0), canvas.transform);
         // Create RightScore GameObject
-        GameObject rightScore = CreateScoreObject("RightScore", new Vector3(100, -100, 0), canvas.transform);
+        CreateScoreObject("RightScore", new Vector3(100, -100, 0), canvas.transform);
     }
 
-    GameObject CreateScoreObject(string name, Vector3 position, Transform parent)
+    void CreateScoreObject(string name, Vector3 position, Transform parent)
     {
+        // Create the parent GameObject
         GameObject scoreObject = new GameObject(name);
         scoreObject.transform.SetParent(parent);
-        scoreObject.transform.localPosition = position;
 
-        TextMeshProUGUI textComponent = CreateTextComponent(scoreObject);
-        textComponent.text = "0";
-
-        return scoreObject;
-    }
-
-    TextMeshProUGUI CreateTextComponent(GameObject parent)
-    {
-        GameObject textObject = new GameObject("Text");
-        textObject.transform.SetParent(parent.transform);
-
-        TextMeshProUGUI textComponent = textObject.AddComponent<TextMeshProUGUI>();
-        RectTransform rectTransform = textComponent.GetComponent<RectTransform>();
+        // Set the RectTransform properties
+        RectTransform rectTransform = scoreObject.AddComponent<RectTransform>();
+        rectTransform.localPosition = position;
         rectTransform.sizeDelta = new Vector2(150, 150);
         rectTransform.anchorMin = new Vector2(0.5f, 1f);
         rectTransform.anchorMax = new Vector2(0.5f, 1f);
-        rectTransform.localPosition = Vector3.zero;
 
+        // Add TextMeshPro component
+        TextMeshProUGUI textComponent = scoreObject.AddComponent<TextMeshProUGUI>();
+        textComponent.text = "0";
         textComponent.enableAutoSizing = true;
         textComponent.fontSizeMin = 18;
         textComponent.fontSizeMax = 250;
-
-        return textComponent;
     }
 }
